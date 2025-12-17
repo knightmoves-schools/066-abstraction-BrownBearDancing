d40907687b9b38ea3aaa90b416f80ea4fabe811a
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-12-17 11:33:44 -0600

    hide

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 class TaxCalculator{
-    #rate = 15;
+    
     constructor(){
-       this.state = 'TX';
+        this.#rate = 15;
+        this.state = 'TX';
         this.exempt = false;
     }
-        #calculateExempt(value) {
-        return value * (this.#rate / 100);
-    }
-
-    #calculateNonExempt(value) {
-        return value * (this.#rate / 100) + 1;
-    }
-    #calculate(){
-        if(this.exempt){
-            return this.#calculateExempt(1.37);
+    
+    calculate(){
+        if(this.exempt) => {
+            return calculateExempt(1.37);
         }else{
-            return this.#calculateNonExempt(5.72);
+            return calculateNonExempt(5.72);
         }
     }
 }
@@ -23,6 +18,7 @@ class TaxCalculator{
 //should hide the rate
 //should hide the calculateExempt method
 //should hide the calculateNonExempt method
+
 
 
 
